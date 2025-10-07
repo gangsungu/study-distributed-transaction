c313d2c34da8d520d59d61971ce4f7fc41641f22
gangsungu <ark_@naver.com>
2025-10-07 23:08:03 +0900

    TCC Retry로 일시적인 오류 대처하기

--- order/src/main/java/org/example/order/config/ApiClientConfig.java
+++ order/src/main/java/org/example/order/config/ApiClientConfig.java
@@ -1,9 +1,11 @@
 package org.example.order.config;
 
+import java.time.Duration;
 import org.example.order.infrastructure.point.PointApiClient;
 import org.example.order.infrastructure.product.ProductApiClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,8 +13,12 @@ public class ApiClientConfig {
 
     @Bean
     public ProductApiClient productApiClient() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(Duration.ofSeconds(2));
+
         return new ProductApiClient(
             RestClient.builder()
+                .requestFactory(factory)
                 .baseUrl("http://localhost:8082")
                 .build()
         );
@@ -20,8 +26,12 @@ public class ApiClientConfig {
 
     @Bean
     public PointApiClient pointApiClient() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(Duration.ofSeconds(2));
+
         return new PointApiClient(
             RestClient.builder()
+                .requestFactory(factory)
                 .baseUrl("http://localhost:8081")
                 .build()
         );
